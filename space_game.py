a9c35dab72c63d3c69156ce0b2496590e1111481
Mr. MacKenty <bmackenty@gmail.com>
2023-12-01 18:17:57 +0100

    changed everything in ways.

--- space_game.py
+++ space_game.py
@@ -1,6 +1,7 @@
 # this is a text-based game where players are space traders
 # and must travel between planets buying and selling goods
-# to make a profit. 
+# to make a profit whilst avoiding dangers and managing their
+# resources, diplomacy and relationships.
 
 
 
@@ -30,98 +31,127 @@ import events
 # import help
 # import settings
 # import credits
-from player import *
+# from player import *
 
-# some functions
-def display_menu():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Welcome to Space Trader!")
-    print("1. Create new character")
-    print("2. Start new game")
-    print("3. Load Game")
-    print("4. Help")
-    print("5. Settings")
-    print("6. Credits")
-    print("7. Quit")
-    choice = input("Enter your choice: ")
-    if choice == "1":
-        new_character()
-    elif choice == "2":
-        new_game()
-    elif choice == "3":
-        load_game()
-    elif choice == "4":
-        help()
-    elif choice == "5":
-        settings()
-    elif choice == "6":
-        credits()
-    elif choice == "7":
-        sys.exit()
-    else:
-        print("Invalid choice. Try again.")
-        display_menu()
+# some classes
 
-def new_character():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Generating new character...")
-    current_player = Player_stats("Boris")
-    Player_stats.greet(current_player)
-    press_enter = input("Press enter to continue.")
-    return current_player
-    
-def new_game():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Starting new game...")
-    press_enter = input("Press enter to continue.")
-    print("=" * 80)
-    print("Welcome to Space Trader!")
-    print("=" * 80)
-    print()
-    print()
-    print("Name: " + current_player.name)
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.id = None
+        self.money = 1000
+        self.ship = None
+        self.cargo = []
+        self.weapons = []
+        self.upgrades = []
+        self.faction = None
+        self.reputation = 0
+        self.skills = {}
+        self.missions = []
+        self.location = None
+        self.traveling = False
+        self.health = 100
+        self.energy = 100
+        self.race = None
+        self.crew = []
+        self.affiliations = []
 
-    press_enter = input("Press enter to continue.")
+    @classmethod
+    def create_new_player(cls, name):
+        return cls(name)
+
+    def update_player(self, **updates):
+        for attribute, value in updates.items():
+            if hasattr(self, attribute):
+                setattr(self, attribute, value)
 
 
+    # Update player's money
+    def add_money(self, amount):
+        self.money += amount
 
+    # Assign a new mission
+    def assign_mission(self, mission):
+        self.missions.append(mission)
 
+    # Print player stats
+    def display_stats(self):
+        print(f"Player: {self.name}")
+        print(f"Money: {self.money}")
+        print(f"Health: {self.health}")
+        # Add more stats as needed
 
+class GameState:
+    def __init__(self):
+        self.player = None
+        # You can add more attributes here as needed, like:
+        # self.current_planet = None
+        # self.game_time = 0
+        # self.global_events = []
 
-# print("There are " + str(len(planets.planets)) + " planets in this universe.")
-# print("There are " + str(len(goods.goods)) + " goods in this universe.")
-# print("There are " + str(len(goods.good_categories)) + " good categories in this universe.")
-# print("There are " + str(len(ships.ship_classes)) + " ship types in this universe.")
-# print("There are " + str(len(ships.ship_upgrades)) + " ship upgrades in this universe.")
-# print("There are " + str(len(stations.space_stations)) + " space station types in this universe.")
-# print("There are " + str(len(markets.markets)) + " markets in this universe.")
-# print("There are " + str(len(factories.factories)) + " factories in this universe.")
-# print("There are " + str(len(pirates.pirates)) + " pirates in this universe.")
-# print("There are " + str(len(organizations.organizations)) + " organizations in this universe.")
-# print("There are " + str(len(missions.missions)) + " missions in this universe.")
-# print("There are " + str(len(events.random_events)) + " events in this universe.")
-# print("There are " + str(len(races.races)) + " races in this universe.")
+    def set_player(self, player):
+        self.player = player
 
-
-def game_loop():
-    # game loop
-    while True:
-        # update_space()
-        # update_combat()
-        # update_diplomacy()
-        # update_trade()
-        # update_travel()
-        # update_economy()
-        # update_politics()
-        # update_missions()
-        # update_events()
-        # update_factories()
-        # update_pirates()
-        # update save()
-        # update_player()
-        display_menu()
+    def update_game_state(self):
+        # This method can be expanded to update various aspects of the game state
+        # For example, updating the game time, handling global events, etc.
         pass
 
+    # Additional methods to manage game state can be added here
+    # Such as methods to change the current planet, update global events, etc.
 
-# start the game
-game_loop()
+
+
+class Main:
+    def __init__(self):
+        self.game_state = GameState()
+        self.running = True
+
+    def start_game(self):
+        # This method could include any initialization logic before the game loop starts
+        print("Welcome to Space Trader!")
+        # For example, creating a new player or loading game data
+        self.game_state.set_player(Player.create_new_player("Commander Shepard"))
+
+    def game_loop(self):
+        while self.running:
+            self.display_menu()
+            self.process_input()
+
+    def display_menu(self):
+        # Display the main menu or game options
+        print("1. New Game")
+        print("2. Load Game")
+        print("3. Exit")
+        # Add more menu options as necessary
+
+    def process_input(self):
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            self.start_new_game()
+        elif choice == "2":
+            self.load_game()
+        elif choice == "3":
+            self.exit_game()
+        else:
+            print("Invalid choice. Please try again.")
+
+    def start_new_game(self):
+        # Logic to start a new game
+        print("Starting new game...")
+        # Potentially call methods from game_state to set up the new game
+
+    def load_game(self):
+        # Logic to load a saved game
+        print("Loading game...")
+        # Implement game loading functionality
+
+    def exit_game(self):
+        print("Exiting game. Thank you for playing!")
+        self.running = False
+
+# Main execution
+if __name__ == "__main__":
+    game = Main()
+    game.start_game()
+    game.game_loop()
