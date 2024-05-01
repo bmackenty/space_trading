42018e0f4dbb6132f509cfe53aaa0adce031d527
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-05-01 10:11:48 +0200

    Update species.py

--- species.py
+++ species.py
@@ -1,3 +1,8 @@
+# As a taxonomic term, race defines an informal subdivision of subspecies which are physically and genetically different.
+# However, the species Homo sapiens cannot be further subdivided into subspecies which are physically and genetically different.
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8604262
+# These are different species:
+
 species = {
     "Zentari": {
         "Appearance": "Blue-skinned, tall, slender",
